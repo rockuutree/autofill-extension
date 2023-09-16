7e8a77dc0286a97881e0edb7bd1f59e501880fb1
rockuutree <114328967+rockuutree@users.noreply.github.com>
2023-09-16 15:57:57 -0700

    Delete Shopify Autofill directory

--- Shopify Autofill/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  var url = chrome.runtime.getURL("pages/index.html");
-  var tab = await chrome.tabs.create({ url });
-  console.log("Created tab ${tab.id}");
-});
